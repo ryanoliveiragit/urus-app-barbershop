2b98954e55a49578b74bd513c88aae0efbcd6622
Ryan Oliveira Brito <ryanoliveirabrito178@gmail.com>
2025-02-03 23:28:45 -0300

    teste api

--- web/app/api/mercado-pago/pending/route.ts
+++ web/app/api/mercado-pago/pending/route.ts
@@ -5,8 +5,9 @@ import mpClient from "@/lib/mercado-pago";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const paymentId = searchParams.get("payment_id");
+  const testeId = searchParams.get("external_reference");
 
-  if (!paymentId) {
+  if (!paymentId || !testeId) {
     return NextResponse.json({ error: "Invalid request" }, { status: 400 });
   }
 
@@ -14,21 +15,13 @@ export async function GET(request: Request) {
     const payment = new Payment(mpClient);
     const paymentData = await payment.get({ id: paymentId });
 
-    // URL base para redirecionamento
-    const baseUrl = "https://urus-app-barbershop-frontend.vercel.app";
-    const redirectUrl = `${baseUrl}?payment_id=${paymentId}`;
-
-    // Verifica se o pagamento foi aprovado
     if (paymentData.status === "approved" || paymentData.date_approved !== null) {
-      return NextResponse.redirect(redirectUrl);
+      return NextResponse.redirect(`/?=${paymentId}`);
     }
 
-    // Caso o pagamento não esteja aprovado, redireciona de qualquer forma
-    return NextResponse.redirect(redirectUrl);
+    return NextResponse.redirect(new URL(`?=${paymentId}`, request.url));
   } catch (error) {
     console.error("Erro ao buscar pagamento:", error);
-    const baseUrl = "https://urus-app-barbershop-frontend.vercel.app";
-    const redirectUrl = `${baseUrl}?payment_id=${paymentId}`;
-    return NextResponse.redirect(redirectUrl);
+    return NextResponse.redirect(new URL(`?=${paymentId}`, request.url));
   }
 }
